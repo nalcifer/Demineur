13a75a01ec054367243a112085646115ef5d02c5
Naëlle PEINADO <n.titanium.e@gmail.com>
2023-02-22 15:00:41 +0100

    input user

--- Demineur/Demineur.c
+++ Demineur/Demineur.c
@@ -30,12 +30,7 @@ int main()
             printf("%d", i + 1);
 
             for (j = 0; j < 5; j++) {
-                if (table_user[i][j] == 'X') {
-                    printf("[X]");
-                }
-                else {
-                    printf("[%c]", table_user[i][j]);
-                }
+                printf("[%c]", table_user[i][j]);
             }
         }
 
@@ -43,24 +38,30 @@ int main()
         int colonne, ligne;
         printf("\nSur quelle case voulez-vous jouer ? :");
         scanf_s("%d %d", &colonne, &ligne);
+        colonne = colonne - 1;
+        ligne = ligne - 1;
 
         //Remplacer case joué dans tableau à afficher
-        if (table_user[colonne][ligne] == 'X' && table_bomb[colonne][ligne] == 'o') { //Si pas de bombe
-            table_user[colonne][ligne] = ' ';
+        if (table_user[colonne][ligne] == 'X') {
+            if (table_bomb[colonne][ligne] == 'o') { //Si pas de bombe
+                table_user[colonne][ligne] = 'O';
+            }
+            else if (table_bomb[colonne][ligne] == 'b') { //Si bombe touché
+                table_user[colonne][ligne] = 'b';
+            }
         }
-        else if (table_bomb == 'b') { //Si bombe touché
-            table_user[colonne][ligne] = 'b';
+        else if (colonne > 5 || ligne > 5) {
+            printf("Rentrez des chiffres entre 1 et 5\n");
         }
-        else {
-            printf("Refaite votre choix\n");
-        }
+        
 
         //Si perdu stop le jeu
         for (i = 0; i < 5; i++) {
             for (j = 0; j < 5; j++) {
                 if (table_user[i][j] == 'b') {
                     condition = 0;
-                    printf("T as perdu miskin");
+                    printf("\nPERDU\n");
+
                 }
             }
         }
