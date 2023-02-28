0d55ec97d857fbf96f993b5e355065014354cd1d
Naëlle PEINADO <n.titanium.e@gmail.com>
2023-02-28 11:21:06 +0100

    euh

--- Demineur/Demineur.c
+++ Demineur/Demineur.c
@@ -5,8 +5,9 @@
 
 void drawTabUser(char tab_user[SIZE][SIZE]);
 void choiceUser(int* colonne, int* ligne);
-void bombInit(int colonne, int ligne, char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE]);
-void end(int tab_bomb[SIZE][SIZE], char tab_user[SIZE][SIZE], int condition);
+int bombInit(int colonne, int ligne, char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE], int bomb_number);
+void clearCases(char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE]);
+int end(int tab_bomb[SIZE][SIZE], char tab_user[SIZE][SIZE], int condition);
 
 int main() {
     srand(time(NULL));
@@ -27,11 +28,25 @@ int main() {
     };
     int i, j;
     int condition = 1;
+    int bomb_number = 0;
    
 
     do {
         //Tracer le tableau
         drawTabUser(tab_user);
+        int i, j;
+        printf(" \n\n");
+        for (i = 0; i < SIZE; i++) {
+            printf("  %d", i + 1);
+        }
+        for (i = 0; i < SIZE; i++) {
+            printf("\n");
+            printf("%d", i + 1);
+
+            for (j = 0; j < SIZE; j++) {
+                printf("[%d]", tab_bomb[i][j]);
+            }
+        }
 
         //Input user choix de case
         int colonne, ligne;
@@ -56,7 +71,7 @@ int main() {
             }
         }
         if (first_attempt == 25) {
-            bombInit(colonne, ligne, tab_user, tab_bomb);
+            bomb_number = bombInit(colonne, ligne, tab_user, tab_bomb, bomb_number);
         }
         else if (tab_user[colonne][ligne] == 'X') {
             if (tab_bomb[colonne][ligne] != 9) { //Si pas de bombe
@@ -66,9 +81,8 @@ int main() {
                 tab_user[colonne][ligne] = 'b';
             }
         }
-
-        
-        
+        //Changer les cases 0 en ' ' et afficher les cases autours
+        clearCases(tab_user, tab_bomb);
 
 
         //Si perdu stop le jeu
@@ -77,15 +91,15 @@ int main() {
             for (j = 0; j < SIZE; j++) {
                 if (tab_user[i][j] == 'b') {
                     printf("\nPERDU\n");
-                    end(tab_bomb, tab_user, condition);
+                    condition = end(tab_bomb, tab_user, condition);
+                }
 
-                    if (tab_user[i][j] != 'X') {
-                        win++;
-                    }
-                    if (win == SIZE * SIZE) {
-                        printf("\nBravo ! Vous avez reussi.\n");
-                        end(tab_bomb, tab_user, condition);
-                    }
+                if (tab_user[i][j] != 'X') {
+                    win++;
+                }
+                if (win == SIZE * SIZE - bomb_number) {
+                    printf("\nBravo ! Vous avez reussi.\n");
+                    condition = end(tab_bomb, tab_user, condition);
                 }
             }
         } 
@@ -117,123 +131,172 @@ void choiceUser (int *colonne, int *ligne) {
 }
 
 
-void bombInit (int colonne, int ligne, char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE]){
+int bombInit (int colonne, int ligne, char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE], int bomb_number){
     int i, j;
     tab_user[colonne][ligne] = ' ';
-        if (colonne < SIZE - 1) {
-            tab_user[colonne + 1][ligne] = ' ';
-        }
-        if (ligne < SIZE - 1) {
-            tab_user[colonne][ligne + 1] = ' ';
-        }
-        if (colonne < SIZE - 1 && ligne < SIZE - 1) {
-            tab_user[colonne + 1][ligne + 1] = ' ';
-        }
-        if (colonne > 0) {
-            tab_user[colonne - 1][ligne] = ' ';
-        }
-        if (ligne > 0) {
-            tab_user[colonne][ligne - 1] = ' ';
-        }
-        if (colonne > 0 && ligne > 0) {
-            tab_user[colonne - 1][ligne - 1] = ' ';
-        }
-        if (colonne < SIZE - 1 && ligne > 0) {
-            tab_user[colonne + 1][ligne - 1] = ' ';
-        }
-        if (colonne > 0 && ligne < SIZE - 1) {
-            tab_user[colonne - 1][ligne + 1] = ' ';
-        }
+    //Pour ne pas mettre de bombes autour du 1er coup
+    if (colonne < SIZE - 1) {
+        tab_user[colonne + 1][ligne] = ' ';
+    }
+    if (ligne < SIZE - 1) {
+        tab_user[colonne][ligne + 1] = ' ';
+    }
+    if (colonne < SIZE - 1 && ligne < SIZE - 1) {
+        tab_user[colonne + 1][ligne + 1] = ' ';
+    }
+    if (colonne > 0) {
+        tab_user[colonne - 1][ligne] = ' ';
+    }
+    if (ligne > 0) {
+        tab_user[colonne][ligne - 1] = ' ';
+    }
+    if (colonne > 0 && ligne > 0) {
+        tab_user[colonne - 1][ligne - 1] = ' ';
+    }
+    if (colonne < SIZE - 1 && ligne > 0) {
+        tab_user[colonne + 1][ligne - 1] = ' ';
+    }
+    if (colonne > 0 && ligne < SIZE - 1) {
+        tab_user[colonne - 1][ligne + 1] = ' ';
+    }
 
-        //Initialisation du tableau bombe
-        int bomb_number = 0;
-        while (bomb_number < SIZE) { //Aléatoire de la position des bombes
-            i = rand() % SIZE;
-            j = rand() % SIZE;
-            if (tab_bomb[i][j] == 0 && tab_user[i][j] == 'X') {
-                tab_bomb[i][j] = 9;
-                bomb_number++;
-            }
+    //Initialisation du tableau bombe
+    while (bomb_number < SIZE) { //Aléatoire de la position des bombes
+        i = rand() % SIZE;
+        j = rand() % SIZE;
+        if (tab_bomb[i][j] == 0 && tab_user[i][j] == 'X') {
+            tab_bomb[i][j] = 9;
+            bomb_number++;
         }
-        //Indices
-        for (i = 0; i < SIZE; i++) {
-            for (j = 0; j < SIZE; j++) {
-                if (tab_bomb[i][j] != 9) {
-                    if (i < SIZE - 1) {
-                        if (tab_bomb[i + 1][j] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+    }
+    //Indices
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (tab_bomb[i][j] != 9) {
+                if (i < SIZE - 1) {
+                    if (tab_bomb[i + 1][j] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (j < SIZE - 1) {
-                        if (tab_bomb[i][j + 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (j < SIZE - 1) {
+                    if (tab_bomb[i][j + 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (i < SIZE - 1 && j < SIZE - 1) {
-                        if (tab_bomb[i + 1][j + 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (i < SIZE - 1 && j < SIZE - 1) {
+                    if (tab_bomb[i + 1][j + 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
+                }
 
-                    if (i > 0) {
-                        if (tab_bomb[i - 1][j] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                if (i > 0) {
+                    if (tab_bomb[i - 1][j] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (j > 0) {
-                        if (tab_bomb[i][j - 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (j > 0) {
+                    if (tab_bomb[i][j - 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (i > 0 && j > 0) {
-                        if (tab_bomb[i - 1][j - 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (i > 0 && j > 0) {
+                    if (tab_bomb[i - 1][j - 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (i < SIZE - 1 && j > 0) {
-                        if (tab_bomb[i + 1][j - 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (i < SIZE - 1 && j > 0) {
+                    if (tab_bomb[i + 1][j - 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
-                    if (i > 0 && j < SIZE - 1) {
-                        if (tab_bomb[i - 1][j + 1] == 9) {
-                            tab_bomb[i][j]++;
-                        }
+                }
+                if (i > 0 && j < SIZE - 1) {
+                    if (tab_bomb[i - 1][j + 1] == 9) {
+                        tab_bomb[i][j]++;
                     }
                 }
             }
         }
-        if (tab_bomb[colonne][ligne] == 9) {
-            tab_user[colonne][ligne] = ' ';
-        }
-        if (colonne < SIZE - 1) {
-            tab_user[colonne + 1][ligne] = tab_bomb[colonne + 1][ligne] + '0';
-        }
-        if (ligne < SIZE - 1) {
-            tab_user[colonne][ligne + 1] = tab_bomb[colonne][ligne + 1] + '0';
-        }
-        if (colonne < SIZE - 1 && ligne < SIZE - 1) {
-            tab_user[colonne + 1][ligne + 1] = tab_bomb[colonne + 1][ligne + 1] + '0';
-        }
-        if (colonne > 0) {
-            tab_user[colonne - 1][ligne] = tab_bomb[colonne - 1][ligne] + '0';
-        }
-        if (ligne > 0) {
-            tab_user[colonne][ligne - 1] = tab_bomb[colonne][ligne - 1] + '0';
-        }
-        if (colonne > 0 && ligne > 0) {
-            tab_user[colonne - 1][ligne - 1] = tab_bomb[colonne - 1][ligne - 1] + '0';
-        }
-        if (colonne < SIZE - 1 && ligne > 0) {
-            tab_user[colonne + 1][ligne - 1] = tab_bomb[colonne + 1][ligne - 1] + '0';
-        }
-        if (colonne > 0 && ligne < SIZE - 1) {
-            tab_user[colonne - 1][ligne + 1] = tab_bomb[colonne - 1][ligne + 1] + '0';
-        }
+    }
+    if (tab_bomb[colonne][ligne] == 9) {
+        tab_user[colonne][ligne] = ' ';
+    }
+    if (colonne < SIZE - 1) {
+        tab_user[colonne + 1][ligne] = tab_bomb[colonne + 1][ligne] + '0';
+    }
+    if (ligne < SIZE - 1) {
+        tab_user[colonne][ligne + 1] = tab_bomb[colonne][ligne + 1] + '0';
+    }
+    if (colonne < SIZE - 1 && ligne < SIZE - 1) {
+        tab_user[colonne + 1][ligne + 1] = tab_bomb[colonne + 1][ligne + 1] + '0';
+    }
+    if (colonne > 0) {
+        tab_user[colonne - 1][ligne] = tab_bomb[colonne - 1][ligne] + '0';
+    }
+    if (ligne > 0) {
+        tab_user[colonne][ligne - 1] = tab_bomb[colonne][ligne - 1] + '0';
+    }
+    if (colonne > 0 && ligne > 0) {
+        tab_user[colonne - 1][ligne - 1] = tab_bomb[colonne - 1][ligne - 1] + '0';
+    }
+    if (colonne < SIZE - 1 && ligne > 0) {
+        tab_user[colonne + 1][ligne - 1] = tab_bomb[colonne + 1][ligne - 1] + '0';
+    }
+    if (colonne > 0 && ligne < SIZE - 1) {
+        tab_user[colonne - 1][ligne + 1] = tab_bomb[colonne - 1][ligne + 1] + '0';
+    }
+    return bomb_number;
 }
 
+void clearCases(char tab_user[SIZE][SIZE], int tab_bomb[SIZE][SIZE]) {
+    int i, j;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (tab_user[i][j] == '0') {
+                tab_user[i][j] = ' ';
+                if (i < SIZE - 1) {
+                        tab_user[i + 1][j] = tab_bomb[i + 1][j] + '0';
+                }
+                if (j < SIZE - 1) {
+                    if (tab_bomb[i][j + 1] != 0) {
+                        tab_user[i][j + 1] = tab_bomb[i][j + 1] + '0';
+                    }
+                }
+                if (i < SIZE - 1 && j < SIZE - 1) {
+                    if (tab_bomb[i + 1][j + 1] != 0){
+                        tab_user[i + 1][j + 1] = tab_bomb[i + 1][j + 1] + '0';
+                    }
+                }
+                if (i > 0) {
+                    if(tab_bomb[i - 1][j] != 0){
+                        tab_user[i - 1][j] = tab_bomb[i - 1][j] + '0';
+                    }
+                }
+                if (j > 0) {
+                    if(tab_bomb[i][j - 1] != 0){
+                        tab_user[i][j - 1] = tab_bomb[i][j - 1] + '0';
+                    }
+                }
+                if (i > 0 && j > 0) {
+                    if(tab_bomb[i - 1][j - 1] != 0){
+                        tab_user[i - 1][j - 1] = tab_bomb[i - 1][j - 1] + '0';
+                    }
+                }
+                if (i < SIZE - 1 && j > 0) {
+                    if (tab_bomb[i + 1][j - 1] != 0){
+                        tab_user[i + 1][j - 1] = tab_bomb[i + 1][j - 1]+ '0';
+                    }
+                }
+                if (i > 0 && j < SIZE - 1) {
+                    if (tab_bomb[i - 1][j + 1] != 0){
+                        tab_user[i - 1][j + 1] = tab_bomb[i - 1][j + 1] + '0';
+                    }
+                }
+            }
+        }
+    }
+}
 
-void end (int tab_bomb[SIZE][SIZE], char tab_user[SIZE][SIZE], int condition) {
+int end (int tab_bomb[SIZE][SIZE], char tab_user[SIZE][SIZE], int condition) {
     int i, j;
     //Tracer tableau en révélant les bombes
     for (i = 0; i < SIZE; i++) {
@@ -259,4 +322,5 @@ void end (int tab_bomb[SIZE][SIZE], char tab_user[SIZE][SIZE], int condition) {
         printf("\nReponse non valide.\nVoulez-vous rejouer ? 1:Oui 0:Non :  ");
         scanf_s("%d", &condition);
     }
+    return condition;
 }
