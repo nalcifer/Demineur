719a1a219756cc5171bb04f6f1b760de2fc5e113
Naëlle <117281037+nalcifer@users.noreply.github.com>
2023-03-05 17:48:05 +0100

    final avec avertissement et bombes en com

--- Demineur/Demineur.c
+++ Demineur/Demineur.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+int row = 0; 
+// initialisation fonctions secondaires
 void drawTabUser(char** tab_user);
 void drawTabBomb(int** tab_bomb);
 int askOneNumber(int min, int max, const char* message);
@@ -11,27 +13,30 @@ void getCoordinatesChoice(int* line, int* column, char** tab_user);
 int initBomb(int line, int column, char** tab_user, int** tab_bomb, int bomb_number);
 void revealSafeBoxes(int line, int column, char** tab_user, int** tab_bomb);
 int isGameOver(int** tab_bomb, char** tab_user, int condition);
-int row = 0; 
 
 int main() {
-    srand(time(NULL));
-
+    system("COLOR 5"); // changement couleur console
+    // initialisation variables
     int condition = 1;
     int first_attempt = 1;
     int i, j;
     char** tab_user = NULL;
     int** tab_bomb = NULL;
     int bomb_number = 0;
+    int win_counter = 0;
+    int defeats_counter = 0;
 
     do {
         system("CLS");
         if (first_attempt == 1) {
-            //--------------------------------------------- Choix taille tableau
-            row = askOneNumber(1, 50, "Entrez la taille du tableau : ");
+            // demande au joueur taille tableau
+            row = askOneNumber(3, 50, "Entrez la taille du tableau (entre 3 et 50): ");
+            if (row == 26) {
+                system("COLOR D7");
+            }
             tab_user = createCharTab(row, 'X');
             tab_bomb = createIntTab(row, 0);
-            //---------------------------------------------
-            //--------------------------------------------- Choix nombre de bombes
+            // choix nombre de bombes
             int bomb_choice = askOneNumber(0, 1, "Voulez-vous choisir le nombre de bombes? 1:OUI 0:NON : ");
             if (bomb_choice == 1) {
             bomb_number = askOneNumber(1, row*row-1, "Entrez le nombre de bombes : ");
@@ -39,14 +44,13 @@ int main() {
             else {
                 bomb_number = row;
             }
-            //---------------------------------------------
         }
         drawTabUser(tab_user);
         /*drawTabBomb(tab_bomb);*/
         
         int line = -1;
         int column = -1;
-        if (first_attempt == 1) { //1er coup
+        if (first_attempt == 1) { // 1er coup
             getCoordinatesChoice(&line, &column, tab_user);
             initBomb(line, column, tab_user, tab_bomb, bomb_number);
             first_attempt = 0;
@@ -54,47 +58,63 @@ int main() {
         else {
             int flag;
             flag = askOneNumber(0, 1, "\n\nVoulez-vous poser un drapeau ? 1:Oui 0:Non :  ");
-            if (flag == 1) { //Drapeaux
+            // jouer un drapeau
+            if (flag == 1) { 
                 getCoordinatesChoice(&line, &column, tab_user);
                 if (tab_user[line][column] == 'X') {
                     tab_user[line][column] = 'D';
                 }
             }
+            // jouer
             else if (flag == 0) {
                 getCoordinatesChoice(&line, &column, tab_user);
                 if (tab_user[line][column] == 'X' || tab_user[line][column] == 'D') {
-                    if (tab_bomb[line][column] != 9) { //Si pas de bombe
+                    if (tab_bomb[line][column] != 9) { // si pas de bombe
                         tab_user[line][column] = tab_bomb[line][column] + '0';
                     }
-                    else if (tab_bomb[line][column] == 9) { //Si bombe touche
+                    else if (tab_bomb[line][column] == 9) { // si bombe touche
                         tab_user[line][column] = 'b';
                     }
                 }
             }
         }
-        //Changer les cases 0 en ' ' et afficher les cases autours
+        // changement cases 0 en [ ] et affichage cases indices autour
         revealSafeBoxes(line, column, tab_user, tab_bomb);
 
-        //Si perdu stop le jeu
+        // fin manche
         int win = 0;
+        int defeat = 0;
         for (i = 0; i < row; i++) {
             for (j = 0; j < row; j++) {
+                // defaite
                 if (tab_user[i][j] == 'b') {
                     printf("\nPERDU\n");
-                    condition = isGameOver(tab_bomb, tab_user, condition);
+                    defeat = 1;
+                    defeats_counter++;
                     first_attempt = 1;
                 }
                 else if (tab_user[i][j] != 'X' && tab_user[i][j] != 'D') {
                     win++;
                 }
-                if (win == row * row - bomb_number) {
-                    printf("\nBravo ! Vous avez reussi.\n");
-                    condition = isGameOver(tab_bomb, tab_user, condition);
-                    first_attempt = 1;
-                }
             }
         }
+        // victoire
+        if (win == row * row - bomb_number) {
+            printf("\nBravo ! Vous avez reussi.\n");
+            win_counter++;
+            first_attempt = 1;
+        }
+        // fin de manche
+        if (defeat == 1 || win == row * row - bomb_number) {
+            printf("\nVictoire(s) : %d     Defaite(s) : %d\n", win_counter, defeats_counter);
+            condition = isGameOver(tab_bomb, tab_user, condition);
+            free(tab_bomb);
+            free(tab_user);
+        }
+
     } while (condition);
+
+    return 0;
 }
 
 
@@ -201,19 +221,20 @@ void getCoordinatesChoice(int* line, int* column, char** tab_user) {
         while (getchar() != '\n');
         (*line)--;
         (*column)--;
-        if (*line > row - 1 || *column > row - 1 || *line < 0 || *column < 0) {
+        if (*line > row - 1 || *column > row - 1 || *line < 0 || *column < 0) { // si mauvais chiffre
             printf("Rentrez des chiffres entre 1 et %d. (ligne.colonne)\n", row);
         }
-        else if (tab_user[*line][*column] != 'X' && tab_user[*line][*column] != 'D') { //Si mauvais chiffre rentre
+        else if (tab_user[*line][*column] != 'X' && tab_user[*line][*column] != 'D') { // si positions pas libre
             printf("\nVous ne pouvez pas jouer sur cette case !\n");
         }
     }
 }
 
 int initBomb(int line, int column, char** tab_user, int** tab_bomb, int bomb_number) {
+    srand(time(NULL));
     int i, j;
     tab_user[line][column] = '0';
-    //Pour ne pas mettre de bombes autour du 1er coup
+    // pour ne pas mettre de bombes autour du 1er coup
     if (line < row - 1) {
         tab_user[line + 1][column] = '0';
     }
@@ -238,9 +259,9 @@ int initBomb(int line, int column, char** tab_user, int** tab_bomb, int bomb_num
     if (line > 0 && column < row - 1) {
         tab_user[line - 1][column + 1] = '0';
     }
-    //Initialisation du tableau bombe
+    // initialisation bombes
     int bomb_in_tab = 0;
-    while (bomb_in_tab < bomb_number) { //Aleatoire de la position des bombes
+    while (bomb_in_tab < bomb_number) { // aleatoire position des bombes
         i = rand() % row;
         j = rand() % row;
         if (tab_bomb[i][j] == 0 && tab_user[i][j] == 'X') {
@@ -248,7 +269,7 @@ int initBomb(int line, int column, char** tab_user, int** tab_bomb, int bomb_num
             bomb_in_tab++;
         }
     }
-    //Autre moyen de placer les bombes en aléatoire
+    //------------------------------------------------------------ Autre moyen de placer les bombes en aléatoire
     /*
     int* bomb_possibilities = malloc(row * row *sizeof(int));
     for (i = 0; i < row; i++) {
@@ -316,8 +337,9 @@ int initBomb(int line, int column, char** tab_user, int** tab_bomb, int bomb_num
         bomb_in_tab++;
     }
     */
-
-    //Indices
+    //------------------------------------------------------------ 
+  
+    // initialisation indices
     for (i = 0; i < row; i++) {
         for (j = 0; j < row; j++) {
             if (tab_bomb[i][j] != 9) {
@@ -434,7 +456,7 @@ void revealSafeBoxes(int line, int column, char** tab_user, int** tab_bomb) {
 
 int isGameOver(int** tab_bomb, char** tab_user, int condition) {
     int i, j;
-    //Tracer tableau en revelant les bombes
+    // tracer tableau en revelant les bombes
     for (i = 0; i < row; i++) {
         for (j = 0; j < row; j++) {
             if (tab_bomb[i][j] == 9) {
@@ -443,14 +465,8 @@ int isGameOver(int** tab_bomb, char** tab_user, int condition) {
         }
     }
     drawTabUser(tab_user);
-    //Rejouer
+    // rejouer
     condition = askOneNumber(0, 1, "\n\nVoulez-vous rejouer ? 1:OUI 0:NON : ");
-    //Remettre les tableaux a 0
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < row; j++) {
-            tab_bomb[i][j] = 0;
-            tab_user[i][j] = 'X';
-        }
-    }
+
     return condition;
 }
